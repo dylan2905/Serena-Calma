d22adb6bfde9c863aed977b6649608b5039a3d02
dylan2905 <110206915+dylan2905@users.noreply.github.com>
2025-08-19 08:35:03 -0500

    Update Combos.js

--- Combos.js
+++ Combos.js
@@ -75,3 +75,12 @@ document.addEventListener('DOMContentLoaded', function() {
         observer.observe(card);
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const navMenu = document.getElementById("nav-menu");
+
+  menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+});
